5a94d1943ba9f0bfe61d21ed0e8d0ebeb9b371da
urohith939 <144365586+urohith939@users.noreply.github.com>
2023-10-31 10:47:17 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -4,7 +4,9 @@ output "ec2_instance_id" {
 output "ec2_instance_public_ip" {
   value = aws_instance.instance-1.public_ip
 }
-
+output "ssh_key_file_name" {
+  value = local_file.ssh_key.filename
+}
 output "elastic_ip" {
   value = aws_eip.demo-eip.public_ip
 }
